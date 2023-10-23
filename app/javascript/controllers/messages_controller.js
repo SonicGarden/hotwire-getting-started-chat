ab067b49457c6a19ebe2f4dace41782c6e0c4f65
yatsuhashi168 <kurimi777luk@gmail.com>
2023-10-23 09:55:00 +0900

    メッセージ作成時に最新メッセージまでスクロールするように

--- app/javascript/controllers/messages_controller.js
+++ app/javascript/controllers/messages_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class extends Controller {
+  static targets = ['broadcastMessage'];
+
   connect() {
     this.element.scrollTop = this.element.scrollHeight;
+  }
+
+  broadcastMessageTargetConnected(element) {
+    element.scrollIntoView();
   }
 }
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,7 +1,7 @@
 class Message < ApplicationRecord
   validates :content, presence: true
 
-  after_create_commit -> { broadcast_append_to('messages', target: 'messages', partial: 'messages/message', locals: { message: self }) }
+  after_create_commit -> { broadcast_append_to('messages', target: 'messages', partial: 'messages/message', locals: { message: self, broadcast: true }) }
   after_update_commit -> { broadcast_replace_to("messages", target: self, partial: 'messages/message', locals: { message: self }) }
   after_destroy_commit -> { broadcast_remove_to("messages", target: self) }
 end
