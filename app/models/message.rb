0a0ab5d0cec765554fca013b8d18bb25f6430a22
yatsuhashi168 <kurimi777luk@gmail.com>
2023-10-23 09:16:34 +0900

    メッセージ編集をリアルタイムに

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,5 @@ class Message < ApplicationRecord
   validates :content, presence: true
 
   after_create_commit -> { broadcast_append_to('messages', target: 'messages', partial: 'messages/message', locals: { message: self }) }
+  after_update_commit -> { broadcast_replace_to("messages", target: self, partial: 'messages/message', locals: { message: self }) }
 end
