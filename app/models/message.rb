a5b6e5af7f0a3a49053020d7681dcc172dec0b14
yatsuhashi168 <kurimi777luk@gmail.com>
2023-10-23 09:26:19 +0900

    メッセージ削除をリアルタイムに

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,5 @@ class Message < ApplicationRecord
 
   after_create_commit -> { broadcast_append_to('messages', target: 'messages', partial: 'messages/message', locals: { message: self }) }
   after_update_commit -> { broadcast_replace_to("messages", target: self, partial: 'messages/message', locals: { message: self }) }
+  after_destroy_commit -> { broadcast_remove_to("messages", target: self) }
 end
