6c3cdff9a290c708ea7da01ffa55150fa251f898
yatsuhashi168 <kurimi777luk@gmail.com>
2023-10-23 09:15:01 +0900

    メッセージ作成をリアルタイムに

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
   validates :content, presence: true
+
+  after_create_commit -> { broadcast_append_to('messages', target: 'messages', partial: 'messages/message', locals: { message: self }) }
 end
